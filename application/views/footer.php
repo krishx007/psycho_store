8c68e2492c005bb286bc084c9faeca0e772bebf7
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-02-27 23:09:35 +0530

    Adding footer, not functioning

---
+++ application/views/footer.php
@@ -0,0 +1,34 @@
+    <footer>
+      <hr>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-push-3 navbar">
+            <ul class="nav nav-pills">
+              <li>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Information</a>
+              </li>
+              <div class="col-md-5">
+                <li>
+                  <div class="input-group">
+                    <input type="text" class="form-control input-sm" placeholder="Be the first to know">
+                    <span class="input-group-btn"><button class="btn btn-warning btn-sm" type="button">Subscribe</button></span>
+                  </div>
+                </li>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
