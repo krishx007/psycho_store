58613e38ad961968bb5b22a384a589059d5523b3
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-03-31 23:11:32 +0530

    proper alignment for catalog view

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -1,26 +1,22 @@
-<div class="container">
-  <div class="well">
-    <div class="row">
-<?php 
-foreach($products as $product_item): 
-	$id = $product_item['product_id'];
-	$path = "/".$product_item['product_image_path'];
-	$image_properties = array(
-          'src' => "$path",          
-          'class' => 'img-responsive',
-);
-?>
-	<div class="col-md-4">
-    <div class="product-link">      
-    	<?php echo anchor("/product/$id", img($image_properties));?>
-    	<div class="row">
-	    	<div class="col-md-12 catalog-desc">
-	    		<h4 class="text-center gothic"> <?php echo $product_item['product_name'] ?> <br> Rs <?php echo $product_item['product_price'] ?></h4>
-	    	</div>
-    	</div>    
-    </div>
-</div>
-<?php endforeach ?>
-    </div>
+<div class="row">
+  <?php 
+  foreach($products as $product_item): 
+  	$id = $product_item['product_id'];
+  	$path = "/".$product_item['product_image_path'];
+  	$image_properties = array(
+            'src' => "$path",          
+            'class' => 'img-responsive',
+  );
+  ?>
+  	<div class="col-md-4">
+      <div class="product-link">      
+      	<?php echo anchor("/product/$id", img($image_properties));?>
+      	<div class="row">
+  	    	<div class="col-md-12 catalog-desc">
+  	    		<h4 class="text-center gothic"> <?php echo $product_item['product_name'] ?> <br> Rs <?php echo $product_item['product_price'] ?></h4>
+  	    	</div>
+      	</div>    
+      </div>
   </div>
+  <?php endforeach ?>
 </div>
--- application/views/home.php
+++ application/views/home.php
@@ -1,11 +1,14 @@
 <div class="container">
-  <div>
-    <div class="row">
-      <div class="col-md-12">
-                <h4 class="lead molot">Shop&nbsp;<small>By</small> <a class = <?php echo $latest_link_state ?> href="latest">Latest</a><small>&nbsp;/&nbsp;</small><a class = <?php echo $popular_link_state ?> href="popular">Popularity</a></h4> 
-      </div>
-    </div>
-  </div>
+	<div>
+	<div class="row">
+	  <div class="col-md-12">
+	            <h4 class="lead molot">Shop&nbsp;<small>By</small> <a class = <?php echo $latest_link_state ?> href="latest">Latest</a><small>&nbsp;/&nbsp;</small><a class = <?php echo $popular_link_state ?> href="popular">Popularity</a></h4> 
+	  </div>
+	</div>
+	</div>
+	<div class="row well">
+		<div class="col-md-12">
+			<?php $data['products'] = $products; $this->load->view('catalog',$data);?>
+		</div>
+	</div>
 </div>
-
-<?php $data['products'] = $products; $this->load->view('catalog',$data);?>
