706a6058b5adb436f8ff3a39f6799bf3872c79e0
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-04-09 20:03:11 +0530

    Minor text changes on launch page

--- application/views/launch_signup.php
+++ application/views/launch_signup.php
@@ -23,13 +23,13 @@
 
 
 <div class="container top-bottom-space">
-	<h1 class=""> Psycho Store <span class="pull-right navbar-text"><small><?php echo $num_of_gamers?> Gamers Found<small></h1>
+	<h1 class=""> Psycho Store <span class="pull-right navbar-text"><small><?php echo $num_of_gamers?> Gamers Found</small></h1>
 	<hr>
 	<div class="well row">
 		<div class="col-md-12">
-			<h3 class="play"><small>We believe that </small> Gamers <small> of this country needs better class of </small>clothing for the Gamer<small> inside them. Rest assured we are upto the task of designing unique clothing inspired by our</small> favorite Video games .</h3>
+			<h3 class="play"><small>We believe that </small> Gamers <small> of this country needs better class of </small>clothing for the Gamer<small> inside them. So sit tight, we are upto the task of designing unique clothing inspired by our</small> favorite Video games.</h3>
 		</div>
-		<div class="col-md-6 col-md-offset-3 top-bottom-space">
+		<div class="col-md-6 col-md-offset-3 top-bottom-space-s">
 			<h4 class=" molot">Join the Gamers League </h4>
             <form class="form" method = "post" action=<?php echo site_url('pages/launch_signup')?>>
 	            <div class="input-group">
@@ -37,6 +37,9 @@
 	            	<span class="input-group-btn"><button class="btn btn-primary btn-sm" type="submit">Yes, I am a GAMER</button></span>
 	            </div>
            </form>            	
+		</div>
+		<div class="col-md-12 text-center navbar-btn top-bottom-space-s">
+			<h2 class="molot">Coming Soon</h2>
 		</div>
 		<div class="col-md-12 text-center">
 			<h4 class="molot">Spread the Psychoness</h4>
