939ba279ec345c04c31559b2bcb1b620c7563d29
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-05-10 20:28:43 +0530

    Removing Lead from home shop by text

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,7 @@
 	<div>
 	<div class="row">
 	  <div class="col-md-12">
-	            <h4 class="lead molot">Shop&nbsp;<small>By</small> <a class = <?php echo $latest_link_state ?> href="latest">Latest</a><small>&nbsp;/&nbsp;</small><a class = <?php echo $popular_link_state ?> href="popular">Popularity</a></h4> 
+	            <h4 class="molot">Shop&nbsp;<small>By</small> <a class = <?php echo $latest_link_state ?> href="latest">Latest</a><small>&nbsp;/&nbsp;</small><a class = <?php echo $popular_link_state ?> href="popular">Popularity</a></h4> 
 	  </div>
 	</div>
 	</div>
