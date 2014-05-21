39c15d53c29ce3bfc4da33e6d222a7384b765c49
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-05-21 21:49:11 +0530

    Setting session expiry to infinite

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = '2ilCp51tB8BvTY7M79d27L7e23zq5Lc0';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= TRUE;
