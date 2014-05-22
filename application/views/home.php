a3025f2d342f25c99b1529c21d83122c8f424738
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-05-22 13:15:17 +0530

    Added self desc on home page

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,16 @@
 <div class="container">
 	<div>
+		<div class="row">
+  			<div class="col-md-12 text-center top-bottom-space-s">
+  				<h1>Psycho Store</h1>
+            	<h4 class="molot"><small>We craft clothing/merchandise for the gaming community of earth</small></h4> 
+  			</div>
+		</div>
+	</div>
 	<div class="row">
 	  <div class="col-md-12">
 	            <h4 class="molot">Shop&nbsp;<small>By</small> <a class = <?php echo $latest_link_state ?> href="latest">Latest</a><small>&nbsp;/&nbsp;</small><a class = <?php echo $popular_link_state ?> href="popular">Popularity</a></h4> 
 	  </div>
-	</div>
 	</div>
 	<div class="row well">
 		<div class="col-md-12">
