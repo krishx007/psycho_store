cdff29be9167a65875dbe7920e7feb147a629626
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-09-11 19:54:18 +0530

    Dont use alpha_dash
    i like < in my passwords

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -77,7 +77,6 @@ class Auth extends CI_Controller
 						$data['login_by_username'],
 						$data['login_by_email'])) {								// success
 					redirect('');
-
 				}
 				else
 				{				
@@ -139,7 +138,7 @@ class Auth extends CI_Controller
 			 $use_username = $this->config->item('use_username', 'tank_auth');
 			if ($use_username) 
 			{				
-				$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean|min_length['.$this->config->item('username_min_length', 'tank_auth').']|max_length['.$this->config->item('username_max_length', 'tank_auth').']|alpha_dash');
+				$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean|min_length['.$this->config->item('username_min_length', 'tank_auth').']|max_length['.$this->config->item('username_max_length', 'tank_auth').']');
 			}
 			$this->form_validation->set_rules('email', 'Email', 'trim|required|xss_clean|valid_email');
 			$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean|min_length['.$this->config->item('password_min_length', 'tank_auth').']|max_length['.$this->config->item('password_max_length', 'tank_auth').']|alpha_dash');
