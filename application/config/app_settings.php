152187935d9d10c2091437b89309ce4e3780414d
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-11-20 02:36:18 +0530

    Adding config files
    Config files for app and payment_gateway vars. Added in autoload.php

---
+++ application/config/app_settings.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+//Default meta tags info
+
+$config['title'] = 'Psycho Store | We make stuff for Gamers and Geeks';
+$config['description'] = 'We make stuff for the gaming/geek community of earth(other planets can wait for now)';
+$config['keywords'] = 't-shirt, tshirt, t shirt, shirt, tee, t, t-shirts, tshirts, t shirts, shirts, tees, ts, clothing, clothes, threads, wear, gift, gifts, hats, hat, beanies, beanie, gear, sweatshirt, hoodie, sweatshirts, hoodies, gamer, geek, hacker, nerd, computer, gamers, geeks, hackers, nerds, coder, coders,';
+
+$config['favico'] = ('images/ps.jpg');
+
+?>
---
+++ application/config/gateway_settings.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+//PayUMoney Settings
+
+$config['merchant_key'] = 'JBZaLc';
+$config['salt'] = 'GQs7yium';
+$config['service_provider'] = 'payu_paisa';
+$config['gateway_url'] = 'https://test.payu.in/_payment/';
+$config['success_url'] = 'http://localhost/psycho_store/checkout/success';
+$config['failure_url'] = 'http://localhost/psycho_store/';
+$config['cancel_url'] = '';
+
+?>
