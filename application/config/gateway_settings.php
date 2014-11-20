93de4c8ab80ef44fe7e05c4ce0ee9200b3929302
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-11-21 03:22:47 +0530

    changing surl/furl

--- application/config/gateway_settings.php
+++ application/config/gateway_settings.php
@@ -6,8 +6,8 @@ $config['merchant_key'] = 'JBZaLc';
 $config['salt'] = 'GQs7yium';
 $config['service_provider'] = 'payu_paisa';
 $config['gateway_url'] = 'https://test.payu.in/_payment/';
-$config['success_url'] = 'http://localhost/psycho_store/checkout/success';
-$config['failure_url'] = 'http://localhost/psycho_store/checkout/failure';
+$config['success_url'] = 'http://psychostore.in/checkout/success';
+$config['failure_url'] = 'http://psychostore.in/checkout/failure';
 $config['cancel_url'] = '';
 
 ?>
