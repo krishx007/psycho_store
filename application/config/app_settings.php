f7375ae04bd3b0ca4a7698dd9877e4733ae3a81b
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-12-08 01:01:23 +0530

    correct path for favico

--- application/config/app_settings.php
+++ application/config/app_settings.php
@@ -7,6 +7,6 @@ $config['title'] = 'Psycho Store | We make stuff for Gamers and Geeks';
 $config['description'] = 'We make stuff for the gaming/geek community of earth(other planets can wait for now)';
 $config['keywords'] = 't-shirt, tshirt, t shirt, shirt, tee, t, t-shirts, tshirts, t shirts, shirts, tees, ts, clothing, clothes, threads, wear, gift, gifts, hats, hat, beanies, beanie, gear, sweatshirt, hoodie, sweatshirts, hoodies, gamer, geek, hacker, nerd, computer, gamers, geeks, hackers, nerds, coder, coders,';
 
-$config['favico'] = ('images/ps.jpg');
+$config['favico'] = 'http://psychostore.in/images/ps.jpg';
 
 ?>
