f0b40cc6b51e08a1026385964de7c8d270801870
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-12-09 01:29:45 +0530

    Submitting mails for order

---
+++ application/views/email/first_order-html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title><?php echo $username?>, your order has been placed!</title></head>
+<body>
+<div style="max-width: 800px; margin: 0; padding: 30px 0;">
+<table width="80%" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td width="5%"></td>
+<td align="left" width="95%" style="font: 13px/18px Arial, Helvetica, sans-serif;">
+<h2 style="font: normal 20px/23px Arial, Helvetica, sans-serif; margin: 0; padding: 0 0 18px; color: black;">Welcome to <?php echo $site_name; ?>!</h2>
+Thanks for joining <?php echo $site_name; ?>. We listed your sign in details below, make sure you keep them safe.<br />
+To open your <?php echo $site_name; ?> homepage, please follow this link:<br />
+<br />
+<br />
+Have fun!<br />
+The <?php echo $site_name; ?> Team
+</td>
+</tr>
+</table>
+</div>
+</body>
+</html>
---
+++ application/views/email/order-html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title><?php echo $username?>, your order has been placed!</title></head>
+<body>
+<div style="max-width: 800px; margin: 0; padding: 30px 0;">
+<table width="80%" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td width="5%"></td>
+<td align="left" width="95%" style="font: 13px/18px Arial, Helvetica, sans-serif;">
+<h2 style="font: normal 20px/23px Arial, Helvetica, sans-serif; margin: 0; padding: 0 0 18px; color: black;">Welcome to <?php echo $site_name; ?>!</h2>
+Thanks for shopping with <?php echo $site_name; ?>. Weare happy to serve you. <br />
+<br />
+<br />
+<nobr><a href="<?php echo site_url('/auth/login/'); ?>" style="color: #3366cc;"><?php echo site_url('/auth/login/'); ?></a></nobr><br />
+<br />
+<?php /* Your password: <?php echo $password; ?><br /> */ ?>
+<br />
+<br />
+Have fun!<br />
+The <?php echo $site_name; ?> Team
+</td>
+</tr>
+</table>
+</div>
+</body>
+</html>
