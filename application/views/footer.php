653ac00b498e4b071e99d04dd95ec86189e0e9cf
codinpsycho <ishkaran.singh@hotmail.com>
2014-12-12 14:36:22 +0530

    Finally Company name approved

--- application/views/footer.php
+++ application/views/footer.php
@@ -71,7 +71,7 @@
         <div class="row">
           <div class="col-md-12">
             <hr>
-            <h5><i class="fa fa-angle-left"></i> <i class="fa fa-angle-right"></i> by Codinpsycho <span class='pull-right'>Psycho Store 2014 &copy; All rights reserved. Product of Psychonet Solutions Opc. Ltd.</span><h5>
+            <h5><i class="fa fa-angle-left"></i> <i class="fa fa-angle-right"></i> by Codinpsycho <span class='pull-right'>Psycho Store 2014 &copy; All rights reserved. Product of Psychonet Solutions Opc Pvt. Ltd.</span><h5>
             <p><small>All artwork posted on this website is intended as fan art and is not purported to be official merchandise. If you have any issues regarding the artwork, please write in to us at contact@psychostore.in</small></p>
           </div>
         </div>
