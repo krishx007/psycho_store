ab6f376e292f5323e5ddd6e4b63bf266459f8ae3
Ishkaran Singh <ishkaran.singh@hotmail.com>
2014-12-14 22:11:57 +0530

    Still changing order mail again

--- application/views/email/order-html.php
+++ application/views/email/order-html.php
@@ -3,19 +3,19 @@
 <head><title>Psycho Store | Order Placed</title></head>
 <body>
 <h1>Hey <?php echo $username?>, your order has been placed!</h1>
+<br>
 <h3>Order Id : <?php echo $order_id; ?></h3>
 <h3>Payment Mode : <?php echo $payment_mode; ?></h3>
 <p>Thank you for shopping at Psycho Store. Our very efficient minions are onto the task of processing your order. So sit back and relax. Orders are usually shipped within 3-5 buisness days unless G-Man gets involved somehow.</p>
 <h2>This awesomeness</h2>
 <?php echo $product_table; ?>
+<br>
 <h2>Will be shipped to this psycho</h2>
 <p> <?php echo $address; ?> </p>
 <br>
 <br>
-
 <p>Note : Please use the above order id in case of any communication/problem regarding this order.</p>
 <br>
-
 Stay Psycho!<br />
 - The <?php echo $site_name; ?> Team
 </div>
