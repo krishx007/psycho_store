ce69174b96c02c288942692cdb00488a2c01aaff
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-02-18 03:26:19 +0530

    Adding web audio experiment

---
+++ application/controllers/wtf.php
@@ -0,0 +1,19 @@
+<?php 
+class Wtf extends CI_controller
+{
+	function __construct()
+	{
+		parent::__construct();		
+	}
+
+	function index()
+	{
+		$this->_start();
+	}
+
+	function _start()
+	{
+		$this->load->view('view_wtf');
+	}
+}
+?>
---
+++ application/views/view_wtf.php
@@ -0,0 +1,3 @@
+<canvas id = "canvas_colors"  style="border:1px solid #d3d3d3;">
+</canvas>	
+<script type="text/javascript" src="Scripts/audio.js"></script>
