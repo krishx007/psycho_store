75bfad2d63eeeb084f50b6d49d3b87ad01b2ddca
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-05-17 12:33:34 +0530

    Correcting typos

--- application/views/success.php
+++ application/views/success.php
@@ -1,7 +1,7 @@
-<br> All right minions, theres work to do, theres stuff to create, people are counting on us, gamers and geeks have high hopes from us and we need to deliver. So stop hunting for bananas and get to work so that this person right here watching us can get what he deserves.<br><br>
-For laymans (seriusly, what are you doing on this site) : Your order has been placed and is up for processing. We do our best to provide you with quality stuff as quickly as possible. A mail has been sent to you confirming the same along with order details.<br><br>
+<br> All right minions, theres work to do, theres stuff to create, people are counting on us, gamers and geeks have high hopes from us and we need to deliver. So stop hunting for bananas and get to work.<br><br>
+For laymans (seriously, what are you doing on this site) : Your order has been placed and is up for processing. We do our best to provide you with quality stuff as quickly as possible. A mail has been sent to you confirming the same along with order details.<br><br>
 
-<p>Following and talking about us on social media makes our minons very happy and more efficient. You should try doing that.</p>
+<p>Following and talking about us on social media makes our minions very happy and more efficient. You should try doing that.</p>
 <div class="row">
 	<div class="col-md-12">		
 		<ul class="nav nav-pills">
