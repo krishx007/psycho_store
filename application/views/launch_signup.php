743f2433d9634dfa2505347186964e4fdc8a3f7b
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-05-19 07:30:24 +0530

    Motherfucker.

--- application/views/launch_signup.php
+++ application/views/launch_signup.php
@@ -27,30 +27,31 @@
 	<hr>
 	<div class="well row">
 		<div class="col-md-12">
-			<h3 class="play"><small>We believe that </small>you Gamers <small>need a different class of clothing to express</small> the Gamer inside you.<small> So sit tight, as we bring you the most amazing collection of clothing inspired by your</small> favorite Video games.</h3>			
+			<h2 class=" text-center">Gamers and Geek merchandises motherfucker.</h2>
+			<h2 class=" text-center"><small>Do you want it ?  </small></h2>
 		</div>
-		<div class="col-md-6 col-md-offset-3 top-bottom-space-s">
-			<h4 class=" molot">Join the Gamers League </h4>
+		<div class="col-md-6 col-md-offset-3 top-bottom-space-s">			
             <form class="form" method = "post" action=<?php echo site_url('pages/launch_signup')?>>
 	            <div class="input-group">
 	            	<input type="email" name="subscribe_email" class="form-control input-sm" placeholder="you@email.com">	            	
-	            	<span class="input-group-btn"><button class="btn btn-primary btn-sm" type="submit">Yes, I am a GAMER</button></span>
+	            	<span class="input-group-btn"><button class="btn btn-primary btn-sm" type="submit">Yes, Take My Money</button></span>
 	            </div>
            </form>
            <h4 class="play"><small>Be the first to know when we go live</small></h4>
 		</div>
-		<div class="col-md-12 text-center navbar-btn top-bottom-space-s">
+		<div class="col-md-12 text-center top-bottom-space-s">
 			<h2 class="molot">Coming Soon</h2>
 		</div>
 		<div class="col-md-12 text-center">
-			<h4 class="molot">Spread the Psychoness</h4>
+			<h4 class="molot">Spread the Word Motherfucker.</h4>
+			<h4 class="molot"><small> Like right now !</small></h4>
 			<iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpsychostorein&layout=button_count&action=like&show_faces=false&share=true&height=21&appId=601282446622582" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px; width:135px;" allowTransparency="true">
             </iframe>
             <a href="https://twitter.com/psychostorein" class="twitter-follow-button" data-show-count="false" data-lang="en" show-screen-name="false" data-size="small">Follow</a>
 		</div>
 	</div>
 	<hr>
-	<h5 class=""><i class="fa fa-angle-left"></i> <i class="fa fa-angle-right"></i> by Codinpsycho <span class='pull-right'>Psycho Store 2014 &copy; All rights reserved!. Product of Psycho Corporation</span><h5>
+	<h5 class=""><i class="fa fa-angle-left"></i> <i class="fa fa-angle-right"></i> by Codinpsycho <span class='pull-right'>Psycho Store 2015 &copy; All rights reserved. Product of Psychonet</span><h5>
 </div>
 
 
