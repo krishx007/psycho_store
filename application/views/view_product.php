d75260465e988eaf056d1cc974d74dd8b37fd7e3
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-06-03 22:39:49 +0530

    Adding product_ntro
    Small description by product side

--- application/views/view_product.php
+++ application/views/view_product.php
@@ -21,6 +21,12 @@
       <div class="col-md-4">
         <div class="row">
           <div class="col-md-12">
+            <p> <?php echo $product['product_intro']; ?> </p>
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
             <p><a class="inline" href="#"><small>Whats my </small><strong> size</strong> ?</a> </p>
           </div>             
           <div class="col-md-4">
