b9eddd659a49bbd7412b86d14520cca9bfea2b2c
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-06-04 20:18:35 +0530

    Always Unique url for disqus
    Just provide product/id .. rest can change .. is optional

--- application/views/header.php
+++ application/views/header.php
@@ -70,9 +70,16 @@
 <body>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
+<?php
+if(isset($product))
+{
+  $disqus_url = base_url()."product/$product[product_id]";
+}
+?>
+
 <script type="text/javascript">  
   var disqus_shortname = 'psychostore';
-  var disqus_url = '<?php echo $url; ?>';
+  var disqus_url = '<?php echo $disqus_url; ?>';
 </script>
 
 </body>
