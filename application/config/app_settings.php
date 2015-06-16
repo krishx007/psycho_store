8b0b99e077a81c2a930e034be73529519929c6fe
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-06-16 15:54:18 +0530

    Changing favico, adding mailgun campaign-id

--- application/config/app_settings.php
+++ application/config/app_settings.php
@@ -7,11 +7,12 @@ $config['title'] = 'Psycho Store | Stuff for Gamers and Geeks';
 $config['description'] = 'We make stuff for the gaming/geek community of earth(other planets can wait for now)';
 $config['keywords'] = 't-shirt, tshirt, t shirt, shirt, tee, t, t-shirts, tshirts, t shirts, shirts, tees, ts, clothing, clothes, threads, wear, gift, gifts, hats, hat, beanies, beanie, gear, sweatshirt, hoodie, sweatshirts, hoodies, gamer, geek, hacker, nerd, computer, gamers, geeks, hackers, nerds, coder, coders, psychostore, psycho, store, psycho store, india, ';
 
-$config['favico'] = 'http://psychostore.in/images/ps.jpg';
+$config['favico'] = 'http://psychostore.in/images/ps.png';
 
 //Admin Panel
 $config['admin_email'] = array('ishkaran.fearme@gmail.com', 'ishkaran.singh@hotmail.com');
 $config['mailgun_key'] = 'key-0f80f2f05eb0cc5a7f733be72c8fb5**';	//old one
 $config['newsletter_address'] = 'updates@news.psychostore.in';
+$config['campaign_id'] = 'psycho_campaign';
 
 ?>
