70c371322992c0ae607187b2677241f22a1417c6
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-06-24 03:12:03 +0530

    Adding config file for shipping

---
+++ application/config/shipping_settings.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//Delhivery
+$config['shipping_url'] = 'test.delhivery.com'
+$config['token'] = 'd553fe2a0286484c51f01e809d5e5ae27c285960';
+$config['warehouse'] = 'PSYCHONETSOLUTIONS - DFS';
+?>
