ad07f42ab4c41931f592ecf564958e22388d2dc9
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-08-05 23:51:07 +0530

    Removing jammu's address

--- application/views/view_contact.php
+++ application/views/view_contact.php
@@ -4,18 +4,14 @@
     <hr> 
     <div class="well">
     	<div class="row ">
-	    	<div class="col-md-12">	    		
+	    	<div class="col-md-12">
 	    		<h5>Feel free to email us at <a href=\"mailto:contact@psychostore.in\">contact@psychostore.in</a> in case of any queries or doubts</h5>
 	    		<h5>Or Just give us a call anytime at +917387045828</h5>
 	    		<h5>Or get in touch through <a class='btn btn-primary' href= <?php echo site_url('auth/saysomething') ?> >Contact Us</a><br><br><br></h5>
 	    		<h4 class="molot">Address for returns</h4>
 				<?php echo $return_address ?>
-	    		<h4 class='molot'>Registered office address</h4>
-	    		<h5>	
-	    			<br>55/2 Nanak Nagar,<br> Lane 1,<br> Jammu - 180004,<br> Jammu and Kashmir<br><br>
-	    		</h5>	    		
 			</div>
-		</div>	
+		</div>
 	</div>
 	<a class='btn btn-primary' href= <?php echo site_url('') ?> >Return To Awesomess</a>
 </div>
