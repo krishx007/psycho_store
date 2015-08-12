98f090f521cd8eef31b2e38ea76a83c244f58eab
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-08-12 17:59:59 +0530

    New test API key Delhivery

--- application/config/shipping_settings.php
+++ application/config/shipping_settings.php
@@ -2,14 +2,14 @@
 
 //Delhivery
 $config['delhivery_url'] = 'http://test.delhivery.com';
-$config['delhivery_token'] = 'd553fe2a0286484c51f01e809d5e5ae27c285960';
-$config['delhivery_warehouse'] = 'PSYCHONETSOLUTIONS - DFS';
+$config['delhivery_token'] = 'b20fa5c4a56ae2d1249bb81957aa5398585d8c91';
+$config['delhivery_warehouse'] = 'PSYCHONETSOLUTIO';
 $config['delhivery_logo'] ='images/shipping/delhivery.png';
 $config['company_logo'] ='images/shipping/shipping_logo.png';
 
 //Pickup Location
 $config['pickup_location'] = array(
-									'add' 	=> '#35, Mane Building, 7th Main Road, Shinivagalu Tank Bed Layout, near back door Balaji Apartments',
+									'add' 	=> '#35, Mane Building, 7th Main Road, Srinivagalu Tank Bed Layout, near back door Balaji Apartments',
 									'city' 	=> 'Bangaluru',
 									'country' => 'India',
 									'name' 	=> $config['delhivery_warehouse'],  // Use client warehouse name
