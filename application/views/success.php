05e3b40ad3769041e46382483ca69ba94a164897
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-08-18 01:52:28 +0530

    Ask for feedback

--- application/views/success.php
+++ application/views/success.php
@@ -1,7 +1,9 @@
 <br> All right minions, theres work to do, theres stuff to create, people are counting on us, gamers and geeks have high hopes from us and we need to deliver. So stop hunting for bananas and get to work.<br><br>
 For laymans (seriously, what are you doing here) : Your order has been placed and is up for processing. We do our best to provide you with quality stuff as quickly as possible. A mail has been sent to you confirming the same along with order details.<br><br>
-
-<p>Following and talking about us on social media makes our minions very happy and more efficient. You should try doing that.</p>
+We hope you had a pleasant time exploring our realm, please let us know your experience.</p>
+<h2><a href=<?php echo site_url('auth/saysomething')?> >Feedback</a></h2>
+<br>
+<p>Also, following and talking about us on social media makes our minions very happy and more efficient. You should try doing that.</p>
 <div class="row">
 	<div class="col-md-12">		
 		<ul class="nav nav-pills">
@@ -20,6 +22,5 @@ For laymans (seriously, what are you doing here) : Your order has been placed an
 		</ul>		
 	</div>
 </div>
-
 <a class= "btn btn-primary" href= <?php echo site_url(''); ?> >Continue Shopping</a>
 
