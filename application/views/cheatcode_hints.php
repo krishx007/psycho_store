dc6f441ee07adf958db0f10f6c4fb65a7310b5ef
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-08-21 22:27:21 +0530

    Dont use spaces

--- application/views/cheatcode_hints.php
+++ application/views/cheatcode_hints.php
@@ -1,4 +1,5 @@
 What's the Konami Code?
 Which mode would you love to have in real life?
-Try a left_right_up_down combination."
+Try a left_right_up_down combination permutation.
+Dont use spaces.
 
