2560a371b1fd02afecf2f95eb21d8ba59770623e
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-08-23 15:48:43 +0530

    Correcting some links and a typo

--- application/views/view_about.php
+++ application/views/view_about.php
@@ -1,4 +1,4 @@
-<h3>Our belief</h3><p>You probably would be thinking why the word "Psycho" in a Gaming Merchandise store. Well it actually has to do with a problem that we have, "Our Belief" that is getting increasingly obsessed with something that we are passionate about. Giving it all that we have and actually going that extra mile when everyone else's common sense says that it's the wrong direction. Did you ever had that feeling that whatever you were doing, your mind was always stuck on that one thing, that one thing which kept knocking on the back door of your mind.
+<h3>Our belief</h3><p>You probably would be thinking why the word "Psycho" in a Gaming Merchandise store. Well it actually has to do with a problem that we have, "Our Belief" that is getting increasingly obsessed with something that we are passionate about. Giving it all that we have and actually going that extra mile when everyone else's common sense says that it's the wrong direction. Did you ever have that feeling that whatever you were doing, your mind was always stuck on that one thing, that one thing which kept knocking on the back door of your mind.
 We call that being in a state of psychoness, in which you feel about something so deep from your heart that you take absurd decisions to get it through, no matter what it takes and whats the cost. If you can relate, then just give us a shout I am sure our frequencies would match.
 In short we have that crazy streak in us for whatever we decide to do. <br>In Hindi we say "keeda hona chahiye kisi cheez ke liye". We just replaced keeda with psycho and we are proud to say that we have that in us.<br>
 Whatever we do, we do with that crazy streak, whoever we work with, we just try to look for that crazy streak in them as well.
@@ -32,6 +32,6 @@ Whatever we do, we do with that crazy streak, whoever we work with, we just try 
 <br>
 <br>
 Kudos to these kickass human beings: 
-<a traget='_blank' href=\"https://twitter.com/mdo\">@mdo</a> and <a traget=\"_blank\" href=\"https://twitter.com/fat\">@fat</a> for <a traget=\"_blank\" href=\"https://getbootstrap.com/\">Bootstrap</a>,
-guys at <a traget='_blank' href=\"http://ellislab.com/\">Ellis Lab</a> for <a traget='_blank' href=\"https://github.com/EllisLab/CodeIgniter/\">CodeIgniter</a></p>
+<a target='_blank' href="https://twitter.com/mdo">@mdo</a> and <a target="_blank" href="https://twitter.com/fat">@fat</a> for <a target="_blank" href="https://getbootstrap.com/">Bootstrap</a>,
+guys at <a target='_blank' href="http://ellislab.com/">Ellis Lab</a> for <a target='_blank' href="https://github.com/EllisLab/CodeIgniter/">CodeIgniter</a></p>
 <p><br>P.S : He also wrote all that stuff you just read, including this line.</p>
