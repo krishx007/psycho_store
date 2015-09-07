cc5cbb280a7258a58b6f4138ce9b53f74ddc6b8f
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-09-08 01:25:42 +0530

    Adding scripts for social plugins

--- application/views/external_scripts.php
+++ application/views/external_scripts.php
@@ -13,6 +13,19 @@ if(isset($product))
   var disqus_url = '<?php echo $disqus_url; ?>';
 </script>
 
+<script>(function(d, s, id)
+{
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=601282446622582";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js"></script>
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
