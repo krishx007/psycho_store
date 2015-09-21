687183a0aedcf7ce5ff632de8531ebc4d7a68e0e
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-09-21 23:34:41 +0530

    Changing button colors

--- application/views/success.php
+++ application/views/success.php
@@ -1,7 +1,7 @@
 <br> All right minions, theres work to do, theres stuff to create, people are counting on us, gamers and geeks have high hopes from us and we need to deliver. So stop hunting for bananas and get to work.<br><br>
 For laymans (seriously, what are you doing here) : Your order has been placed and is up for processing. We do our best to provide you with quality stuff as quickly as possible. A mail has been sent to you confirming the same along with order details.<br><br>
 We hope you had a pleasant time exploring our realm, please let us know your experience.
-<a class="btn btn-danger"href=<?php echo site_url('auth/saysomething')?> >Feedback</a>
+<a class="btn btn-primary"href=<?php echo site_url('auth/saysomething')?> >Feedback</a>
 <p>Also, following and talking about us on social media makes our minions very happy and more efficient. You should try doing that.</p>
 <div class="row">
 	<div class="col-md-12">		
@@ -18,5 +18,5 @@ We hope you had a pleasant time exploring our realm, please let us know your exp
 		</ul>		
 	</div>
 </div>
-<a class= "btn btn-primary" href= <?php echo site_url(''); ?> >Continue Shopping</a>
+<a class= "btn btn-default" href= <?php echo site_url(''); ?> >Continue Shopping</a>
 
