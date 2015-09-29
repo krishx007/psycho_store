340421db072541fc8facd725047891b7b6943d4a
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-09-30 02:32:46 +0530

    Decouple disqus script and view

--- application/views/view_disqus.php
+++ application/views/view_disqus.php
@@ -3,15 +3,5 @@
 	    <h5>Lets Discuss '<?php echo $product_name?>' here or you can send us <a href= <?php echo site_url('auth/saysomething')?> >feedback</a>
 	    </h5>
 	    <hr>
-		<div id="disqus_thread"></div>
-	  		<script type="text/javascript">
-		    /* * * DON'T EDIT BELOW THIS LINE * * */
-		    (function() {
-		        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-		        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-		    })();
-			</script>
-			<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
-	</div>
+	    <?php $this->view('disqus_script')?>		
 </div>
