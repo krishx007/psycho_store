1090991c21c04e7635fa14d9ca4d24481b16d0d2
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-10-06 21:57:54 +0530

    target="_blank"

--- application/views/view_media.php
+++ application/views/view_media.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-md-4">
-		<a href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">
+		<a target="_blank" href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">
 			<img class="img-responsive" src= <?php echo site_url('images/media/gc.jpg')?>  alt="Interview with Psycho Store founder Ishkaran Singh">
 		</a>
 	</div>
@@ -10,7 +10,7 @@
 		<p>Gaming accessories are tough to come by in India and importing them usually costs a lot more than what my meager wallet can bear. So when I first met Psycho Store's founder, Ishkaran Singh, his idea for bringing in gaming merchandise and accessories to India excited me, especially so considering it wasn't burning a hole in my wallet! A couple of months later, I had another chance to meet Ishkaran, but this time I had a pen and paper.<br><br>
 
 		Read on for his experiences in gaming and his steps to becoming a brand owner!
-		<a href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">Read more </a>
+		<a target="_blank" href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">Read more </a>
 		</p>
 	</div>
 </div>
