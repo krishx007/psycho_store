afd0cc49cf200fd551817888a619ba04afba49f7
Ishkaran Singh <ishkaran.singh@hotmail.com>
2015-10-06 21:55:34 +0530

    Adding Gamingcentral link in media

--- application/views/view_media.php
+++ application/views/view_media.php
@@ -1,1 +1,16 @@
-<h3 class=" play text-center">No one</h3>
+<div class="row">
+	<div class="col-md-4">
+		<a href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">
+			<img class="img-responsive" src= <?php echo site_url('images/media/gc.jpg')?>  alt="Interview with Psycho Store founder Ishkaran Singh">
+		</a>
+	</div>
+	<div class="col-md-8">
+		<h4 class="">Gamingcentral</h4>
+		<h5>Interview with Psycho Store founder Ishkaran Singh : </h5>
+		<p>Gaming accessories are tough to come by in India and importing them usually costs a lot more than what my meager wallet can bear. So when I first met Psycho Store's founder, Ishkaran Singh, his idea for bringing in gaming merchandise and accessories to India excited me, especially so considering it wasn't burning a hole in my wallet! A couple of months later, I had another chance to meet Ishkaran, but this time I had a pen and paper.<br><br>
+
+		Read on for his experiences in gaming and his steps to becoming a brand owner!
+		<a href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">Read more </a>
+		</p>
+	</div>
+</div>
